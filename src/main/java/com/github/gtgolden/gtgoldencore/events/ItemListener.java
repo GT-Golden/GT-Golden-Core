d47a83167bf44aa82d895c68f8b88b2d90d980d2
Bleb1k <viktor.masnikov17283940@gmail.com>
2023-03-21 21:28:10 +0900

    removed testing materials

--- src/main/java/com/github/gtgolden/gtgoldencore/events/ItemListener.java
+++ src/main/java/com/github/gtgolden/gtgoldencore/events/ItemListener.java
@@ -13,26 +13,11 @@ import static com.github.gtgolden.gtgoldencore.item.MetaItem.MISSING;
 public class ItemListener {
     @EventListener
     public void registerItems(ItemRegistryEvent event) {
-        LOGGER.info("Registering technical stuff");
+        LOGGER.info("Registering technical items and materials");
         MISSING = new MetaItem(MOD_ID.id("missing"));
 
         Materials.useModID(MOD_ID);
 
         new GTMaterial.Builder("").build();
-        new GTMaterial.Builder("copper")
-                .element("Cu")
-                .build();
-        new GTMaterial.Builder("oxygen")
-                .element("O")
-                .build();
-        new GTMaterial.Builder("hydrogen")
-                .element("H")
-                .build();
-        new GTMaterial.Builder("cuprous_oxide")
-                .components("Cu2O")
-                .build();
-        new GTMaterial.Builder("copper2_hydroxide")
-                .components("Cu(OH)2")
-                .build();
     }
 }
